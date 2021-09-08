4ee928502315dcdb724908746c7b9ddde167e4fc
FrostBid <83489811+FrostBid@users.noreply.github.com>
2021-09-08 16:35:15 +0800

    Finalising twitter.py

--- README.rst
+++ README.rst
@@ -11,8 +11,8 @@ Twitter stock sentinment: Compares twitter sentinment with the real performance 
 Info
 --------
 
-* Compares Twitter sentinment with the real pricing of a stock from 2019 Jan - 2021 June.
-* Stock list: AAPL, AAL, GME, MARA, TSLA.
+* Shows overall sentinment of the latest 5000 tweets of stocks on Twitter.
+* Shows wordcloud.
 * **This is not for scientific research purposes** but merely as an exercise to gain experience on managing a project structure, the use of APIs, VADER Analysis, pandas/matlab visualisation tools as well as working with financial data via python.
 * To be featured on a portfolio website using django.
 
@@ -26,6 +26,7 @@ The project has also taken reference from the following github pages/websites.
 * https://github.com/evayl2/Twitter-Stock-Sentiment-Analysis
 * https://towardsdatascience.com/sentiment-analysis-for-stock-price-prediction-in-python-bed40c65d178
 * https://www.analyticsvidhya.com/blog/2018/07/hands-on-sentiment-analysis-dataset-python/
+* https://github.com/ariessa/Twitter-Sentiment-Analysis-Using-VADER
 
 .. _Cookiecutter: https://github.com/audreyr/cookiecutter
 .. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
--- twitter_stock/twitter.py
+++ twitter_stock/twitter.py
@@ -19,10 +19,12 @@ class TwitterData():
         auth.set_access_token(accesstoken, accesstokensecret)
         api = tweepy.API(auth)
         self.results = []
-        print('Twitter authentication completed, fetching tweets...')
+        print('Twitter authentication successful, fetching tweets...')
         for tweet in tqdm(tweepy.Cursor(api.search, q=f'{self.stock} -filter:retweets min_faves:1', tweet_mode='extended', lang='en',
-                                   wait_on_rate_limit=True).items(20), total=20):
+                                   wait_on_rate_limit=True).items(2500), total=2500):
           tweet.text = str(tweet.full_text.lower())
+
+          # Filter out spam and tweets from small twitter accounts
           if tweet.user.followers_count > 25 and tweet.text.count('$') <= 5:
               self.results.append(tweet)
 
@@ -37,7 +39,7 @@ class TwitterData():
         self.data_set["date"] = [tweet.created_at for tweet in self.results]
         self.data_set["follower_count"] = [tweet.user.followers_count for tweet in self.results]
 
-        # Data cleaning
+        # Data cleaning (Remove links, twitter handles and extra spaces)
         self.data_set['text'] = self.data_set['text'].str.replace('http\S+|www.\S+', '', regex=True)
         self.data_set['text'] = np.vectorize(self.remove_pattern)(self.data_set['text'], "@[\w]*")
         self.data_set['text'] = self.data_set['text'].str.replace(r'\n', ' ', regex=True)
@@ -45,7 +47,7 @@ class TwitterData():
         self.data_set['text'] = self.data_set['text'].str.decode("utf-8")
         return self.data_set
 
-    def remove_pattern(self,input_txt, pattern):
+    def remove_pattern(self, input_txt, pattern):
       r = re.findall(pattern, input_txt)
       for i in r:
         input_txt = re.sub(i, '', input_txt)
@@ -53,10 +55,11 @@ class TwitterData():
 
     def csv(self):
         # Export to csv
-        self.data_set.to_csv('scraped_user_tweets.csv')
+        self.data_set.to_csv(f'{self.stock}_tweets.csv')
         return self.data_set
 
     def wordcloud(self):
+        # Generate a wordcloud
         all_words = ' '.join([text for text in self.data_set['text']])
         wordcloud = WordCloud(width=800, height=500, random_state=21, background_color='white',
                               max_font_size=110).generate(all_words)
@@ -64,3 +67,4 @@ class TwitterData():
         plt.imshow(wordcloud, interpolation="bilinear")
         plt.axis("off")
         plt.show()
+        plt.savefig(f'{self.stock} wordcloud.png')
